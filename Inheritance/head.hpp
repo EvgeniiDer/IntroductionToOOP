f10346fe0069a37cd554861aac6a85b637c6cd27
EvgeniiDer <128982133+EvgeniiDer@users.noreply.github.com>
2024-06-06 23:00:21 +1000

    HomeWork

---
+++ Inheritance/head.hpp
@@ -0,0 +1,5 @@
+#ifndef HEAD_HPP_
+#define HEAD_HPP_
+#include<iostream>
+
+#endif
---
+++ Inheritance/main.cpp
@@ -0,0 +1,7 @@
+#include"head.hpp"
+
+int main()
+{
+    std::cout << "Hellow World" << std::endl;
+    return 0;
+}
